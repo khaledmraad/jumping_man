c0ae6a0a0f1991e4017ce2a274cfce7550cd0e56
khaleedmrad <khaled.mrad@ensi-uma.tn>
2024-04-14 00:38:28 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController player;
+    public float sensitivity = 200f;
+    private float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.localEulerAngles.y;
+    }
+
+    private void Update()
+    {
+        Look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
+
+    private void Look()
+    {
+        float mouseVertical = Input.GetAxis("Mouse Y");
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        this.verticalRotation += mouseVertical * this.sensitivity * Time.deltaTime;
+        this.horizontalRotation += mouseHorizontal * this.sensitivity * Time.deltaTime;
+
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -this.clampAngle, this.clampAngle);
+
+        this.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0f, 0f);
+        this.player.transform.rotation = Quaternion.Euler(0f, this.horizontalRotation, 0f);
+    }
+}
